abc58817318b42e48797c5aa547ed9aa08e3e7ef
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-15 15:59:57 +0300

    Бесконечные последовательности

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -2,14 +2,10 @@ package kot
 
 
 fun main(args: Array<String>) {
-    val array = (0..1000).toList()
-    val employees = array.map { "Employee #$it" }
-    val first30 = employees.take(30)
-    for (employee in first30){
-        println(employee)
+    val array = generateSequence("Сотрудник №1"){
+        val index = it.substring(11).toInt()
+        "Сотрудник №${index+1}"
     }
-    val dropfirst30 = employees.drop(30)
-    for (employee in dropfirst30){
-        println(employee)
-    }
+    val listOfFirst100 = array.take(100)
+    for(employee in listOfFirst100) println(employee)
 }
