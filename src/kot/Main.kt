e7c90c6904905dabc598c8fb00706fbf5a3bc966
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-15 15:53:18 +0300

    take/drop

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -2,5 +2,14 @@ package kot
 
 
 fun main(args: Array<String>) {
-    val
+    val array = (0..1000).toList()
+    val employees = array.map { "Employee #$it" }
+    val first30 = employees.take(30)
+    for (employee in first30){
+        println(employee)
+    }
+    val dropfirst30 = employees.drop(30)
+    for (employee in dropfirst30){
+        println(employee)
+    }
 }
