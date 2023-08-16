ccd2c203a292cbb73cbceaea9472a093a266fe74
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-16 14:48:58 +0300

    Метод flatten(flatMap)

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -8,12 +8,7 @@ fun main(args: Array<String>) {
         listOf(7,3,4,2,1),
         listOf(1,4,5,7,6)
     )
-    val total = mutableListOf<Int>()
-    revenueByWeek.map {
-        for (i in it){
-            total.add(i)
-        }
-    }
+    val total = revenueByWeek.flatten()
     val average = total.average()
     println(average)
 }
