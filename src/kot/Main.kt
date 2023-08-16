6dbb0c7b320d8371a6ebd6eca4a65a4fcb39bd71
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-16 14:47:54 +0300

    Метод average

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -2,14 +2,18 @@ package kot
 
 
 fun main(args: Array<String>) {
-    val fullNames = mutableListOf<String>()
-    for(i in 0..1000){
-        fullNames.add("Имя $i, Фамилия $i")
+    val revenueByWeek = listOf(
+        listOf(8,6,5,1,2),
+        listOf(2,4,6,1,3),
+        listOf(7,3,4,2,1),
+        listOf(1,4,5,7,6)
+    )
+    val total = mutableListOf<Int>()
+    revenueByWeek.map {
+        for (i in it){
+            total.add(i)
+        }
     }
-    val names = fullNames.map{ it.substringBefore(",")}
-    val lastNames= fullNames.map{ it.substringAfter(",")}
-    val users = names.zip(lastNames)
-    for(user in users){
-        println(user.first + user.second)
-    }
+    val average = total.average()
+    println(average)
 }
