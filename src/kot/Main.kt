24557e0ab192e6fcf791e468f5748422dd036cf5
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-16 16:43:22 +0300

    Своя extention функция с дженериками

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -1,22 +1,44 @@
 package kot
 
 
-
 fun main(args: Array<String>) {
-    val age = 5
-    if(age.isPositive()){
-        println("Valid")
+    val list = listOf<Int>()
+    val string = "fgdsfsdf"
+    val obj = mutableMapOf<String, String>()
+    myWith(list) {
+        sum()
+        average()
     }
+    myWith(string) {
+        println(this.toUpperCase())
+    }
+    myWith(obj) {
+        keys
+        values
+    }
+
+
 }
 
+//fun myWith(list:List<Int>, operation:List<Int>.()->Unit){
+//    list.operation()
+//}
+//
+//fun myWith(string: String, operation:String.()->Unit){
+//    string.operation()
+//}
+
+inline fun <T, R> myWith(obj: T, operation: T.() -> R): R {
+    return obj.operation()
+}
 
 fun Int.isAgeValid() = this in 6..100
 
-fun Int.isPositive() = this >=0
+fun Int.isPositive() = this >= 0
 
-fun Int.isPrime():Boolean{
-    if(this<=3) return true
-    for(i in 2 until this) {
+fun Int.isPrime(): Boolean {
+    if (this <= 3) return true
+    for (i in 2 until this) {
         if (this % i == 0) return false
     }
     return true
