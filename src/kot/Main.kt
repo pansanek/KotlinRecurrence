c9c6a79d503ad00cfff3f5a968f9eac81fcf845b
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-16 16:18:27 +0300

    Extension-функции

--- src/kot/Main.kt
+++ src/kot/Main.kt
@@ -3,10 +3,21 @@ package kot
 
 
 fun main(args: Array<String>) {
-    val result = modifyString(listOf(1,2,3,4,5)){it.sum()}
-    println(result)
+    val age = 5
+    if(age.isPositive()){
+        println("Valid")
+    }
 }
 
-fun modifyString(list: List<Int>,modify:(List<Int>) ->Int):Int{
-    return modify(list)
+
+fun Int.isAgeValid() = this in 6..100
+
+fun Int.isPositive() = this >=0
+
+fun Int.isPrime():Boolean{
+    if(this<=3) return true
+    for(i in 2 until this) {
+        if (this % i == 0) return false
+    }
+    return true
 }
