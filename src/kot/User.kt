8f2ff40282edea50f817a79cce4ea94ba94f2dbb
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-17 14:43:48 +0300

    Введение в ООП

---
+++ src/kot/User.kt
@@ -0,0 +1,16 @@
+package kot
+
+class User {
+    var age: Int = 0
+    set(value) {
+        if(value>=0){
+            field=value
+        }
+    }
+    var name: String? = null
+    get() {
+        return if(field == null){
+            ""
+        } else field
+    }
+}
