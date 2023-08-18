5ebd942fd4410f2e8a8a4904804e4f0c619dd0b2
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-18 21:34:38 +0300

    Equals

---
+++ src/jav/Address.java
@@ -0,0 +1,47 @@
+package jav;
+
+public class Address {
+    private String city;
+    private String street;
+    private int number;
+
+    public Address(String city, String street, int number) {
+        this.city = city;
+        this.street = street;
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Address) {
+            Address address = (Address) obj;
+            return this.city.equals(address.city) && this.street.equals(address.street) && this.number == address.number;
+        }
+        return false;
+    }
+}
--- src/jav/Main.java
+++ src/jav/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
 
     public static void main(String[] args) {
-       for(int i =0;i<=100;i++){
-           System.out.println(i);
-       }
-
-
+//        Address address1 = new Address("Moscow", "Green", 150);
+//        Address address2 = new Address("Moscow", "Green", 150);
+//        if(address1==address2){
+//            System.out.println("Equal");
+//        }
+        Address address1 = new Address("Moscow", "Green", 150);
+        Address address2 = address1;
+        if(address1.equals(address2)){
+            System.out.println("Equal");
+        }
     }
 }
